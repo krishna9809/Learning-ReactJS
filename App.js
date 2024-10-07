29d6953c07105810b00a05d37a7c7a1f570c7b36
krishna9809 <multiatoms980996@gmail.com>
2024-10-08 00:59:58 +0530

    Completed Episode 1-inception - Amazing Episode

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 
 const parent = React.createElement("div", { id: "parent"}, 
     React.createElement("child", { id: "child"}, 
-        React.createElement("h1", {id: "happy"}, "Hello I am Tree Like Stucture")
+        [React.createElement("h1", {id: "happy"}, "Hello I am Tree Like Stucture"), React.createElement("h2", {id: "happy_2"}, "Ho I am Tree Like Stucture")]
      ))
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
