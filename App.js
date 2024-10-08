ddcca6e441f9889cbfaa2ced87fe1346eea6ecab
krishna9809 <multiatoms980996@gmail.com>
2024-10-08 11:31:33 +0530

    react is Amazing

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 // div div div child
 
-const parent = React.createElement("div", { id: "parent"}, 
-    React.createElement("child", { id: "child"}, 
-        [React.createElement("h1", {id: "happy"}, "Hello I am Tree Like Stucture"), React.createElement("h2", {id: "happy_2"}, "Ho I am Tree Like Stucture")]
-     ))
+const parent = React.createElement("div", {id: "Parent"}, 
+  [React.createElement("div", {id: "child"}, 
+    React.createElement("h1", {id: "lets"}, "Hello World")
+  ), React.createElement("div", {id: "child2"}, 
+    React.createElement("h1", {id: "lets"}, "Hello eeWorld"))]
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
