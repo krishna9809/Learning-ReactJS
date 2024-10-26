5da17491dc3dd165fa709536991ca748a21f5a24
krishna9809 <multiatoms980996@gmail.com>
2024-10-26 22:23:39 +0530

    Episode-3

--- App.js
+++ App.js
@@ -2,16 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: "Parent"}, 
-  [React.createElement("div", {id: "child"}, 
-    React.createElement("h1", {id: "lets"}, "Hello World")
-  ), React.createElement("div", {id: "child2"}, 
-    React.createElement("h1", {id: "lets"}, "Hello eeWorld"))]
-)
+// React Element
 
+const heading = React.createElement("h1", {id: "heading" }, "Namastey");
+
+//JSX
+
+const jsxheading = <h1>Namestey in JSX</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-
-krosn
+root.render(jsxheading);
